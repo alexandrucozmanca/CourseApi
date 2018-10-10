37e4c44c1d8c807fee85e1c30add98692d9a90e3
alexandru.cozmanca <alexandru.cozmanca@gmail.com>
2018-10-10 09:28:41 +0300

    Topics Service with hardcoded output

---
+++ src/main/java/ro/alex/springbootquickstart/service/TopicService.java
@@ -0,0 +1,21 @@
+package ro.alex.springbootquickstart.service;
+
+import org.springframework.stereotype.Service;
+import ro.alex.springbootquickstart.domain.Topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = Arrays.asList(
+            new Topic("spring", "Spring Framework", "SpringFramework Description"),
+            new Topic("java", "Core Java", "CoreJava Description"),
+            new Topic("javascript", "Java Script", "Java Script Description")
+    );
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+}
