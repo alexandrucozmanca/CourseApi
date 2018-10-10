816ad32725c65fb7b7abc24306e24db78e681564
alexandru.cozmanca <alexandru.cozmanca@gmail.com>
2018-10-10 12:11:41 +0300

    TopicRepository

---
+++ src/main/java/ro/alex/springbootquickstart/repositories/TopicRepository.java
@@ -0,0 +1,8 @@
+package ro.alex.springbootquickstart.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import ro.alex.springbootquickstart.domain.Topic;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
